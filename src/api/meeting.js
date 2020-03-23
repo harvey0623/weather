aa21481d6ceaeadc4f1c56632d005977f42997b3
Harvey <harvey@henghung.com>
2020-03-23 18:02:51 +0800

    修改apiurl

--- src/api/meeting.js
+++ src/api/meeting.js
@@ -2,6 +2,12 @@ import httpMethod from './http.js';
 
 export default class Meeting {
    static getMeetingList(option = {}) {
-      return httpMethod({ url: '/meetinglist', method: 'get' }, option);
+      return httpMethod({ 
+         url: process.env.VUE_APP_GASP,
+         method: 'get',
+         params: {
+            url: 'https://opendata.cwb.gov.tw/webapi/announcement/meetinglist'
+         }
+      }, option);
    }
 }

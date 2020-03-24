ed7c5b976ac795932a2f7fea2fb5f5a40e1becb1
Harvey <harvey@henghung.com>
2020-03-24 09:54:39 +0800

    移除ajax上不要的參數

--- src/api/meeting.js
+++ src/api/meeting.js
@@ -1,12 +1,12 @@
 import httpMethod from './http.js';
 
 export default class Meeting {
-   static getMeetingList(option = {}) {
+   static getMeetingList() {
       const url = 'https://opendata.cwb.gov.tw/webapi/announcement/meetinglist';
       return httpMethod({ 
          url: process.env.VUE_APP_GASP,
          method: 'get',
          params: { url }
-      }, option);
+      });
    }
 }
